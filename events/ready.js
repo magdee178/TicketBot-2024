0142bf6ec4e0685df2887cb6f22ca96c3428b7ac
magdee178 <150984583+magdee178@users.noreply.github.com>
2024-03-16 19:10:07 +0200

    ready.js

--- events/ready.js
+++ events/ready.js
@@ -23,8 +23,8 @@ console.log('\x1b[32m%s\x1b[0m', `|    🌼 Logged in as ${client.user.username}
 
 const serverCount = client.guilds.cache.size;
 setInterval(() => client.user.setActivity({ 
-  name:`Tickets`, 
-  type: ActivityType.Listening }), 10000);
+  name:`ZAX Tickets`, 
+  type: ActivityType.Playing }), 10000);
 client.errorLog = config.errorLog
   
 }
